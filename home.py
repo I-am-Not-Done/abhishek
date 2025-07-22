8126aaef2ccb754b4db43dcf4127f2308bec3682
ABHISHEK ALLAMPELLI <abhishekallampelli123@gmail.com>
2025-07-22 11:27:57 +0530

    Update home.py

--- home.py
+++ home.py
@@ -1,44 +1,65 @@
-'''a=[1,2,3,4,1,2,3,12,3,33,]
-s=set(a)
-print(s)
-if 4 in s:
-    print("Element is present")
-else:
-    print("Not prsesnt in list")'''
-'''a=['b','c','d','e','a','s','d','a','s']
-s=set(a)
-print(s)'''
-'''for i in range(2,10,2):
-    print(i)
-for i in range(1,11):
-    print(i,end=" ")'''
-'''i=1
-while i<=2:
-      print(i)'''
-'''a=[1,2,1,2,1,3,2,4,3,2,3]
-s=len(a)
-target=1
-count=0
-for i in range(s):
-    if a[i]==target:
-        count+=1
-    print(count)'''
-'''a=[1,2,1,2,1,3,2,4,3,2,3]
-d={}
-for i in a:
-    if i in d :
-        d[i]+=1
-    else:
-        d[i]=1
-    print(d)'''
-a=[1,2,1,2,1,3,2,73,4,3,2,3]
-maxi=a[0]
-for i in a:
-    if i>maxi:
-        maxi=i
-print(maxi)
+import cv2
+import dlib
+import pyautogui
+import numpy as np
 
+# Load face detector and shape predictor
+detector = dlib.get_frontal_face_detector()
+predictor = dlib.shape_predictor("shape_predictor_68_face_landmarks.dat")  # Download separately
 
+# Get screen size
+screen_width, screen_height = pyautogui.size()
 
+# Define indexes for left and right eyes
+LEFT_EYE_POINTS = list(range(36, 42))
+RIGHT_EYE_POINTS = list(range(42, 48))
 
- 
+# Get eye center from landmarks
+def get_eye_center(landmarks, eye_points):
+    eye_region = np.array([(landmarks.part(point).x, landmarks.part(point).y) for point in eye_points])
+    x = int(np.mean(eye_region[:, 0]))
+    y = int(np.mean(eye_region[:, 1]))
+    return (x, y)
+
+# Webcam
+cap = cv2.VideoCapture(0)
+
+while True:
+    ret, frame = cap.read()
+    if not ret:
+        break
+
+    frame = cv2.flip(frame, 1)  # Mirror image
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    faces = detector(gray)
+
+    for face in faces:
+        landmarks = predictor(gray, face)
+
+        left_eye_center = get_eye_center(landmarks, LEFT_EYE_POINTS)
+        right_eye_center = get_eye_center(landmarks, RIGHT_EYE_POINTS)
+
+        # Midpoint between both eyes
+        eyes_center = ((left_eye_center[0] + right_eye_center[0]) // 2,
+                       (left_eye_center[1] + right_eye_center[1]) // 2)
+
+        # Map coordinates from webcam to screen size
+        rel_x = eyes_center[0] / frame.shape[1]
+        rel_y = eyes_center[1] / frame.shape[0]
+
+        screen_x = int(screen_width * rel_x)
+        screen_y = int(screen_height * rel_y)
+
+        pyautogui.moveTo(screen_x, screen_y)
+
+        # Draw circles on eyes
+        cv2.circle(frame, left_eye_center, 3, (0, 255, 0), -1)
+        cv2.circle(frame, right_eye_center, 3, (0, 255, 0), -1)
+
+    cv2.imshow("Eye Controlled Cursor", frame)
+
+    if cv2.waitKey(1) == 27:  # Press ESC to exit
+        break
+
+cap.release()
+cv2.destroyAllWindows()
